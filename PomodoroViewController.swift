3b2a2826071749d3c6f7f2ce6b22a52e3bdf8e7a
kamilcaglar <alarkamil34@gmail.com>
2022-08-30 14:37:35 +0200

    play sound method added validation bugs has been solved.

--- PomodoroViewController.swift
+++ PomodoroViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PomodoroViewController: UIViewController {
     
@@ -13,30 +14,28 @@ class PomodoroViewController: UIViewController {
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var resetButton: UIButton!
     
-    
+    var player: AVAudioPlayer?
     var myTimer = Timer()
+    var mysTimer = Timer()
     var isTimerStarted = false
-    var time = 1500
-    
+    var workTime = 3 //1500
+    var breakTime = 300
     
     @IBOutlet weak var timerLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     
-    @IBAction func startButton(_ sender: Any) {
+    @IBAction func startButton(_ sender: UIButton) {
         
         resetButton.isEnabled = true
-        resetButton.alpha = 1.0
-        if !isTimerStarted{
-            
+        
+        if !isTimerStarted {
             startTimer()
             isTimerStarted = true
             startButton.setTitle("Pause", for: .normal)
-            startButton.setTitleColor(UIColor.orange, for: .normal)
-            
+            startButton.setTitleColor(UIColor.purple, for: .normal)
             
         }else {
             myTimer.invalidate()
@@ -44,15 +43,14 @@ class PomodoroViewController: UIViewController {
             startButton.setTitle("Resume", for: .normal)
             startButton.setTitleColor(UIColor.green, for: .normal)
         }
-        
     }
     
     @IBAction func resetButton(_ sender: Any) {
-        //resetButton.isEnabled = false
-        resetButton.alpha = 0.5
-        //myTimer.invalidate()
+        resetButton.isEnabled = false
+        myTimer.invalidate()
+        mysTimer.invalidate()
         //time = 1500
-        //isTimerStarted = false
+        isTimerStarted = false
         //timeLabel.text = "25:00"
         
         let alert = UIAlertController(title: "Reset Timer?", message: "Are you sure you would like to reset the Timer?", preferredStyle: .alert)
@@ -60,10 +58,11 @@ class PomodoroViewController: UIViewController {
             //do nothing
         }))
         alert.addAction(UIAlertAction(title: "YES", style: .default, handler: { (_) in
-            self.time = 1500
             self.myTimer.invalidate()
+            self.mysTimer.invalidate()
+            self.workTime = 1500
             self.timeLabel.text = "25:00"
-            self.startButton.setTitle("START", for: .normal)
+            self.startButton.setTitle("Start", for: .normal)
             self.startButton.setTitleColor(UIColor.blue, for: .normal)
             self.resetButton.setTitleColor(UIColor.green, for: .normal)
         }))
@@ -75,16 +74,59 @@ class PomodoroViewController: UIViewController {
         myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(updateTimer)), userInfo: nil, repeats: true)
     }
     
-    @objc func updateTimer(){
-        //fix when time < 1
-        time -= 1
-        timeLabel.text = formatTime()
-        // add new future there is a app timer app git copy code for alert
+    func startBreakTime() {
+        mysTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(makeBreak)), userInfo: nil, repeats: true)
     }
     
-    func formatTime()->String{
-        let minutes = Int(time) / 60 % 60
-        let seconds = Int(time) % 60
+    @objc func makeBreak() {
+        breakTime -= 1
+        timeLabel.text = formatBreakTime()
+    }
+    
+    @objc func updateTimer(){
+        //fix when time < 1
+        workTime -= 1
+        if workTime == 0{
+            playSound()
+            myTimer.invalidate()
+            startBreakTime()
+            timeLabel.text = "05:00"
+            startButton.setTitle("Take a break", for: .normal)
+        }
+        timeLabel.text = formatWorkTime()
+    }
+    
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "damn", withExtension: "mp3") else { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            
+            /* iOS 10 and earlier require the following line:
+             player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+            
+            guard let player = player else { return }
+            
+            player.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func formatBreakTime() -> String{
+        let minutes = Int(breakTime) / 60 % 60
+        let seconds = Int(breakTime) % 60
+        return String(format: "%02d:%02d", minutes,seconds)
+    }
+    
+    func formatWorkTime()->String{
+        let minutes = Int(workTime) / 60 % 60
+        let seconds = Int(workTime) % 60
         return String(format:"%02d:%02d", minutes, seconds)
         
     }
